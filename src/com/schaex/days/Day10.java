f8c62009d066d8f1a9dddd3b16d5a2f26f48e6e8
Schaex <53149887+Schaex@users.noreply.github.com>
2024-12-10 20:04:03 +0100

    Day 10: Shortened main method significantly by only doing one round of iteration

--- src/com/schaex/days/Day10.java
+++ src/com/schaex/days/Day10.java
@@ -54,42 +54,27 @@ public class Day10 {
     }
 
     public static void main(String... args) {
-        // Collect pairs that actually lead to each other
-        final List<Pair<Point, Point>> startEndPairs = new ArrayList<>();
+        int countPart1 = 0; // 737
+        int countPart2 = 0; // 1619
 
-        System.out.print("Part one: ");
+        int intermediary;
 
-        // 737
-        {
-            for (Point start : STARTS) {
-                for (Point end : ENDS) {
-                    // We don't need to check if the points are too far away from each other
-                    if (start.taxicabDistance(end) > 9) {
-                        continue;
-                    }
+        for (Point start : STARTS) {
+            for (Point end : ENDS) {
+                // We don't need to check if the points are too far away from each other
+                if (start.taxicabDistance(end) > 9) {
+                    continue;
+                }
 
-                    if (start.canReach(end)) {
-                        startEndPairs.add(new Pair<>(start, end));
-                    }
+                if ((intermediary = start.numberOfTrailsTo(end)) > 0) {
+                    countPart1++;
+                    countPart2 += intermediary;
                 }
             }
-
-            System.out.println(startEndPairs.size());
         }
 
-        System.out.print("Part two: ");
-
-        // 1619
-        {
-            int count = 0;
-
-            // We can now simply iterate over the list we built earlier
-            for (Pair<Point, Point> pair : startEndPairs) {
-                count += pair.left().numberOfTrailsTo(pair.right());
-            }
-
-            System.out.println(count);
-        }
+        System.out.println("Part one: " + countPart1);
+        System.out.println("Part two: " + countPart2);
     }
 
     // Container for 2D-coordinates
@@ -102,60 +87,27 @@ public class Day10 {
             return Math.abs(x - other.x) + Math.abs(y - other.y);
         }
 
-        boolean canReach(Point other) {
-            return other.canBeReached(x, y, '1');
-        }
-
-        boolean canBeReached(int currentX, int currentY, int nextHeight) {
-            int nextX, nextY;
-
-            for (Direction direction : Direction.values()) {
-                // If we are in bounds of the map and the next coordinates are not too far away from the goal
-                if (ParamUtil.isInRange(nextX = currentX + direction.dx, 0, WIDTH)  &&
-                    ParamUtil.isInRange(nextY = currentY + direction.dy, 0, HEIGHT) &&
-                    taxicabDistance(nextX, nextY) <= ('9' - nextHeight)) {
-
-                    // Is this the correct height?
-                    if (MAP[nextY][nextX] != nextHeight) {
-                        continue;
-                    }
-
-                    // Did we reach the end?
-                    if (nextHeight == '9') {
-                        return true;
-                    }
-
-                    // Recursive call with the new coordinates and the next height
-                    if (canBeReached(nextX, nextY, nextHeight + 1)) {
-                        return true;
-                    }
-                }
-            }
-
-            // We checked all directions but none passed all checks
-            return false;
-        }
-
         int numberOfTrailsTo(Point other) {
             return other.numberOfTrails(x, y, '1');
         }
 
-        // This method is very similar to canBeReached(). However, it counts all the paths
         int numberOfTrails(int currentX, int currentY, int nextHeight) {
             int count = 0;
 
             int nextX, nextY;
 
             for (Direction direction : Direction.values()) {
+                // If we are in bounds of the map and the next coordinates are not too far away from the goal
                 if (ParamUtil.isInRange(nextX = currentX + direction.dx, 0, WIDTH)  &&
                         ParamUtil.isInRange(nextY = currentY + direction.dy, 0, HEIGHT) &&
                         taxicabDistance(nextX, nextY) <= ('9' - nextHeight)) {
 
+                    // Is this the correct height?
                     if (MAP[nextY][nextX] != nextHeight) {
                         continue;
                     }
 
-                    // Add 1 to the counter of the previous call (from nextHeight = '8')
+                    // At the end -> Add 1 to the counter of the previous call (from nextHeight = '8')
                     if (nextHeight == '9') {
                         return 1;
                     }
